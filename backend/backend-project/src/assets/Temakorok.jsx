dc8426d52f8d69beedf3728cfa55698d9a7bc2ef
retkesbegre <115552479+retkesbegre@users.noreply.github.com>
2025-09-22 11:18:51 +0200

    a

--- backend/backend-project/src/assets/Temakorok.jsx
+++
@@ -1,5 +0,0 @@
-const Temakorok = () => {
-    return <div>Témakörök</div>;
-  };
-  
-  export default Temakorok;
---
+++ raboczkibuta/src/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return(
+        <>
+        Hello én vagyok a fő oldal
+        </>
+    )
+}
+
+export default Home;
